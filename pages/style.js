5b50057abf3029923bdac0990fbba9b33b42ed9e
LuckyFantomass <stibik0930@seznam.cz>
2022-04-09 15:34:18 +0200

    almost_done

--- pages/style.js
+++ pages/style.js
@@ -1,43 +1,47 @@
-swal({
-    title: "UPOZORNĚNÍ!",
-    text: "Stránka je ve vývoji, makáme na ní!",
-    icon: "info",
-  });
+(function ($) {
 
 
-var icons = $('.fa'),
-    styles = {
-        position: 'relative',
-        zIndex: 10,
-    },
-    settings = { top: 200, right: 150 },
-    speed = 1000;
+    var icons = $('.fa'),
+        styles = {
+            position: 'relative',
+            zIndex: 10,
+        },
+        settings = { top: 200, right: 150 },
+        speed = 1000;
         
 
 
-icons.on('mouseenter', function () {
-    icons.css(styles)
-    $(this).animate(settings, speed)
+    icons.on('mouseenter', function () {
+        icons.css(styles)
+        $(this).animate(settings, speed)
+    
     });
 
 
-$('main').hide().fadeIn(1000);
-$('.citat_levy').hide().fadeToggle(5000);;
+    $('main').hide().fadeIn(1000);
+    $('.citat_levy').hide().fadeToggle(5000);;
 
 
 
 
 
-$('.mainMenu').find('a').on('click', function(event) {
+    $('.mainMenu').find('a').on('click', function (event) {
 	
-	$(this)
-		.addClass('selected')
-        .siblings().removeClass('selected');
+        $(this)
+            .addClass('selected')
+            .siblings().removeClass('selected');
         
 
-});
+    });
 
 
+    swal({
+        title: "UPOZORNĚNÍ!",
+        text: "Stránka je ve vývoji, makáme na ní!",
+        icon: "info",
+    })
+    
+})(jQuery);
 
 
 
