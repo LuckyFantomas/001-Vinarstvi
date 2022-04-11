bd15ae7cbc2a3f0a8520d64c7acb71ab694a26f8
LuckyFantomass <stibik0930@seznam.cz>
2022-04-11 12:36:40 +0200

    prettier, editorconfig

--- pages/style.js
+++ pages/style.js
@@ -1,6 +1,4 @@
 (function ($) {
-
-
     var icons = $('.fa'),
         styles = {
             position: 'relative',
@@ -8,71 +6,24 @@
         },
         settings = { top: 200, right: 150 },
         speed = 1000;
-        
-
 
     icons.on('mouseenter', function () {
-        icons.css(styles)
-        $(this).animate(settings, speed)
-    
+        icons.css(styles);
+        $(this).animate(settings, speed);
     });
-
 
     $('main').hide().fadeIn(1000);
-    $('.citat_levy').hide().fadeToggle(5000);;
+    $('.citat_levy').hide().fadeToggle(5000);
 
-
-
-
-
-    $('.mainMenu').find('a').on('click', function (event) {
-	
-        $(this)
-            .addClass('selected')
-            .siblings().removeClass('selected');
-        
-
-    });
-
+    $('.mainMenu')
+        .find('a')
+        .on('click', function (event) {
+            $(this).addClass('selected').siblings().removeClass('selected');
+        });
 
     swal({
-        title: "UPOZORNĚNÍ!",
-        text: "Stránka je ve vývoji, makáme na ní!",
-        icon: "info",
-    })
-    
+        title: 'UPOZORNĚNÍ!',
+        text: 'Stránka je ve vývoji, makáme na ní!',
+        icon: 'info',
+    });
 })(jQuery);
-
-
-
-
-
-
-
-
-
-
-
-/*
-var a = +prompt("Enter first number");
-var b = 3;
-
-alert(a * b);
-
-
-function nasob(a, b) {
-    return a * b
-}
-var vysledek = nasob (a, b)
-console.log(vysledek);*/
-
-/*var a = +prompt("Enter first number");
-var b = +prompt("Enter second number");
-alert(a + b);
-
-
-function scitej(a, b) {
-    return a + b
-}
-var vysledek = scitej (a, b)
-console.log (vysledek)*/
